9046d5b73c8407bc21d310fd748837199e767a1c
Antonio Lanzolla <Antonio.Lanzolla@wolterskluwer.com>
2020-06-17 09:01:37 +0200

    First commit

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilesController extends Controller
+{
+    //
+    public function show()
+    {
+        return response()->download(storage_path('app\Diario_delle_attivita_.pdf'), 'activity');
+    }
+
+    public function create(Request $request)
+    {
+        $path = $request->file('photo')->store('testing');
+        return response()->json(['path' => $path, 200]);
+    }
+}
